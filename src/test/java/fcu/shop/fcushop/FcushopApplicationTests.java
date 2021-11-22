07d904c7449143319f9fefc460ac087f73c02230
chengyu <yces4199600@gmail.com>
2021-11-22 11:12:14 +0800

    hello

---
+++ src/test/java/fcu/shop/fcushop/FcushopApplicationTests.java
@@ -0,0 +1,13 @@
+package fcu.shop.fcushop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcushopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
